1208f22fb9cafe5c5ac94dc34643a1ab8414247d
Gala3G <galagallardogarcia@gmail.com>
2023-01-20 00:05:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Listado from './components/Listado.vue';
+</script>
+
+<template>
+<Listado/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Listado.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue';
+const nombre = ref('');
+const nombres = ref([]);
+const listado = ref(false)
+
+
+
+const guardar = () => {
+    if (nombre.value == '') {
+        window.alert('Nombre no puede ser vacío');        
+    }
+    else {
+        window.alert('se guarda nombre');
+        nombres.value.push(nombre.value);
+        nombre.value = '';
+    }
+}
+
+const listar = () => {
+    listado.value = true; 
+    if (listado.value==true) {
+        console.log('Listar'); 
+    }
+}
+
+const nolistar = ()=> {
+    listado.value = false; 
+}
+
+const borrar = () => {
+    nombres.value=[]
+}
+</script>
+
+<template>
+    <input class="form-control" v-model="nombre" placeholder="Introduce tu nombre">
+    <button @click="guardar" class="btn btn-secondary mt-2 mx-2">Guardar</button>
+    <button @click="listar" class="btn btn-warning mt-2 mx-2">Listar</button>
+    <button @click="nolistar" class="btn btn-primary mt-2 mx-2">No Listar</button>
+    <button @click="borrar" class="btn btn-close mt-2 mx-2"></button>
+
+    <div v-if="listado">
+        <ul v-for="item in nombres">
+            <li>{{ item }}</li>
+        </ul>        
+    </div>
+</template>
+
+
+    
+<style>
+@import url(../assets/boostrap.css);
+</style>
